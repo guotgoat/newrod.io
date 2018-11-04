e2f22106b2be60676e8a6c622dd54dc8d29aee82
guotgoat <guotgoat@github.com>
2018-11-04 03:03:21 -0500

    fu

---
+++ rod.io/Assets/Scenes/Collision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Collision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+
+    {
+        Debug.Log("Collided with " + collider.gameObject.name);
+        if (collider.gameObject.tag == "Capsule(1)")
+        {
+            Destroy(collider.gameObject);
+
+        }
+
+    }
+}
